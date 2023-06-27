e7ac3217666f5931c9b81d94239e88b7fb5a451f
Felicia572 <feliciachan572@gmail.com>
2023-06-27 17:17:30 +0800

    測試controllers

---
+++ Controllers/Blog_Detail_Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Asp_Net_FinalProject.Controllers
+{
+    public class Blog_Detail_Controller : Controller
+    {
+        // GET: Blog_Detail_
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
